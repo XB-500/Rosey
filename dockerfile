c44b6c98cca2e99ffc8cf63df945d6621bbca946
Justin Joyce <justin.joyce@joocer.com>
2021-04-27 21:33:29 +0100

    first app

---
+++ dockerfile
@@ -0,0 +1,17 @@
+FROM python:3-slim AS build-env
+ADD . /app
+WORKDIR /app
+
+COPY src app
+COPY requirements.txt /temp/
+RUN pip install --target=/app -r /temp/requirements.txt
+RUN useradd -u 8877 gvauser
+
+FROM gcr.io/distroless/python3
+COPY --from=build-env /app /app
+
+EXPOSE 6147
+
+USER gvauser
+WORKDIR /app
+CMD ["main.py"]
---
+++ src/main.py
@@ -0,0 +1,55 @@
+import uvicorn
+from fastapi import FastAPI  # type:ignore
+from fastapi.responses import UJSONResponse  # type:ignore
+from fastapi import Request
+from mabel.logging import get_logger, set_log_name
+
+set_log_name("ROSEY")
+logger = get_logger()
+app = FastAPI()
+
+
+@app.post("/hook")
+def recWebHook(req: Request):
+    try:
+        body = req.json()
+        
+        github_event = req.headers.get("X-Github-Event")
+
+        request_id = abs(hash(body))
+        logger.debug(F"Started ID: {request_id} - {github_event}")
+        
+
+        if github_event == "star":  # check if the event is a star
+            nos_stars = body["repository"]["stargazers_count"]
+            starrer_username = body["sender"]["login"]
+            repo_url = body["repository"]["html_url"]
+            repo_name = body["repository"]["name"]
+            message = f"{starrer_username} has starred the [{repo_name}]({repo_url}). \n\n The Total Stars are {nos_stars}"
+
+        elif github_event == "pull_request":  # check if event is a pull request
+            pr_number = body["number"]
+            if body["pull_request"]["merged"] == True:
+                pr_action = "merged"
+            pr_action = body["action"]
+            pr_title = body["pull_request"]["title"]
+            pr_desc = body["pull_request"]["body"]
+            pr_login = body["sender"]["login"]
+            pr_login_url = body["sender"]["html_url"]
+            pr_url = body["pull_request"]["html_url"]
+            message = f"Pull Request([{pr_number}]({pr_url})) {pr_action} by [{pr_login}]({pr_login_url}).\n\n Title: *{pr_title}* \n\n Description: **{pr_desc}**"
+            
+        logger.info(message)
+    
+    except Exception as err:
+        error_message = F"{type(err).__name__}: {err} (ID:{request_id})"
+        logger.error(error_message)
+        return {}, 500
+    finally:
+        logger.debug(F"Finished ID:{request_id}")
+
+    return {}, 200
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=6147)
