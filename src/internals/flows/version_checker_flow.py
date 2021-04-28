1b0e5cf02e256245990aa33385d251a503b437db
Justin Joyce <justin.joyce@joocer.com>
2021-04-28 20:57:23 +0100

    update

--- src/internals/flows/version_checker_flow.py
+++ src/internals/flows/version_checker_flow.py
@@ -1,7 +1,11 @@
 from mabel.operators import EndOperator
 
+VERSION_FILE_URL = "https://raw.githubusercontent.com/joocer/mabel/{branch}/mabel/version.py"
+
 def version_checker_flow():
 
+    # extract_details
+    # retrieve_version_file
+    # submit push if the versions  don't match
+
     end = EndOperator()
-
-
