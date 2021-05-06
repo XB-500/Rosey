18dace9aa01f5c05c0c3a139ba710d4831408d50
Justin Joyce <justin.joyce@joocer.com>
2021-05-06 20:29:07 +0100

    README

---
+++ src/timed.py
@@ -0,0 +1,10 @@
+
+
+# go through all the repos of an org
+# is the repo from the template
+#    download the repo
+#    download the template
+#    for each file in the template
+#        if not on a whitelist and different
+#            add to a branch
+#    if there are any changes,submit pull request
