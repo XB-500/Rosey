68d6b06ec33772982728e9a97ef1191ec79b2a7b
joocer <justin.joyce@joocer.com>
2021-06-09 21:54:27 +0100

    debug

--- src/internals/operators/get_repos_operator.py
+++ src/internals/operators/get_repos_operator.py
@@ -24,6 +24,6 @@ class GetReposOperator(BaseOperator):
         self.logger.debug(f"I found {len(repo_list)} repositories.")
 
         for repo in repo_list:
-            print(type(repo), )
+            print(type(repo))
             print(repo)
             yield repo, context
